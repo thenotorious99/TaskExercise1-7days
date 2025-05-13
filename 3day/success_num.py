5cd7c9fb5459829d4ede8be7c27d445309790508
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2025-05-12 22:10:32 -0500

    Day3

---
+++ 3day/success_num.py
@@ -0,0 +1,7 @@
+num = int(input("Enter Number: "))
+
+while num != 100:
+    print("Wrong number. Try again!")
+    num = int(input("Enter Number: "))
+
+print(f"Success! Success number is: {num}.")
